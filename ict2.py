80520c54deaabbddfc6472cac57f62f59964af04
Karen <garciakj8@gmail.com>
2026-06-19 14:10:46 +1000

    Initial Project Setup

---
+++ ict2.py
@@ -0,0 +1,16 @@
+students = []
+
+while True:
+    print("1. Add Student")
+    print("2. View Students")
+
+
+    choice = input("Choice:")
+
+    if choice == "1" :
+        name = input ("Name: ")
+        students.append(name)
+
+    elif choice == "2":
+        print(students)
+        
